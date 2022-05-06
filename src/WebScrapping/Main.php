bec73f1a9de4d0096205a2bceb144435c7456049
Isaque Menezes <isaque-menezes@hotmail.com>
2022-05-06 17:44:34 -0300

    upload

---
+++ src/WebScrapping/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Galoa\ExerciciosPhp2022\WebScrapping;
+
+use DOMDocument;
+
+/**
+ * Runner for the Webscrapping exercice.
+ */
+class Main {
+
+  /**
+   * Main runner, instantiates a Scrapper and runs.
+   */
+  public static function run(): void {
+    
+      $dom = new DOMDocument('1.0', 'utf-8');
+      
+      $dom->loadHTMLFile(__DIR__ . '/../../webscrapping/origin.html');
+
+      (new Scrapper())->scrap($dom);
+  }
+
+}
